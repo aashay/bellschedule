60e3c4a7948034afc57546bc2f1ba0bf7b3aeeed
Aashay Desai <aashayd@gmail.com>
2014-09-03 12:40:31 -0700

    Use static middleware

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
+var serveStatic = require('serve-static');
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.use(serveStatic(__dirname + '/public/html'));
+
+app.get('/foo', function(req, res){
+  res.send('This is foo, great.');
 });
-
-
-
-
 
 var port = parseInt(process.env.PORT) || 5000;
 app.listen(port, function() {
